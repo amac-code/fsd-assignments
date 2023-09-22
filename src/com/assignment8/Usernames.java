35e3e2061fa863b2fa69a49fbfc7a616d9705b66
amac-code <dr.anshumanmac@outlook.com>
2023-09-22 19:05:16 +0530

    added usernames.java

--- src/com/assignment8/Usernames.java
+++ src/com/assignment8/Usernames.java
@@ -1,29 +1,45 @@
-/*package com.assignment8;
+package com.assignment8;
 
-
-
-import java.util.Scanner;
-
-// Problem 4: Write a program that uses a list to store user names.
+// Problem 4: Write a program that uses a list to store usernames.
 // Implement a method to add a new name to the list, and another method to check if a given name exists in the list.
 // If the name exists, return "Name found," otherwise return "Name not found."
+
+
+import java.util.*;
+
 public class Usernames {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("How many name you want to enter?: ");
+        ArrayList<String> usernames = new ArrayList<>();
+
+        System.out.print("How many names do you want to enter?: ");
         int num = sc.nextInt();
+        sc.nextLine(); // Consume the newline character
 
+        // Adding new names
+        addNewNames(usernames, num);
+
+        // Checking for existing names
+        System.out.print("Enter the name you want to search for: ");
+        String searchName = sc.nextLine();
+        String result = checkName(usernames, searchName);
+        System.out.println(result);
     }
 
-    public static int newName(int num){
+    public static void addNewNames(ArrayList<String> usernames, int num) {
         Scanner sc = new Scanner(System.in);
-        String[] name= new String[num];
         for (int i = 0; i < num; i++) {
-            System.out.println("Enter name " + (i+1) + ": ");
-            name[i] = sc.nextLine();
+            System.out.print("Enter name " + (i + 1) + ": ");
+            String name = sc.nextLine();
+            usernames.add(name);
         }
-
-
-        return num;
     }
-}*/
+
+    public static String checkName(ArrayList<String> usernames, String searchName) {
+        if (usernames.contains(searchName)) {
+            return "Name found";
+        } else {
+            return "Name not found";
+        }
+    }
+}
