0aebae66df394bd4385eccb4e66cb9ee6a66f68e
amac-code <dr.anshumanmac@outlook.com>
2023-09-22 18:14:21 +0530

    Committing all local changes

---
+++ src/com/assignment9/StringManipulation.java
@@ -0,0 +1,34 @@
+/*
+package com.assignment9;
+
+*/
+/*
+* Problem 1: String Manipulation
+           * Write a Java program that takes a user input string and then performs the following operations:
+           * Print the length of the string.
+           * Convert the string to uppercase and print it.
+           * Check if the string contains the word "Java" (case-insensitive) and print "Contains 'Java'" or "Does not contain 'Java'" accordingly.
+*
+* *//*
+
+
+import java.util.Scanner;
+
+public class StringManipulation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the string: ");
+        String str = sc.nextLine();
+
+        System.out.println("Length of String is: " + str.length());
+
+        String upperCase= str.toUpperCase();
+        System.out.println("Upper case of String is: " + upperCase);
+
+        boolean check = false;
+        String target = "Java";
+
+
+    }
+}
+*/
