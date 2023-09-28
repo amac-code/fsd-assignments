119074826451ff2cc1271e4f5c6bd49f207a95ab
Dr. Ansuman Machahary <139002352+amac-code@users.noreply.github.com>
2023-09-28 15:03:02 +0530

    Added SimpleMultiplication

---
+++ src/com/assignment11/SimpleMultiplication.java
@@ -0,0 +1,14 @@
+package com.assignment11;
+import java.util.Scanner;
+
+//Problem Statement.
+//This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+public class SimpleMultiplication{
+    public static void main (String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+    }
+}
